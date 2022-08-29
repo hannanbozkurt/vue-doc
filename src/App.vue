da86f8dbeef7a3dde5213fed8848368e559394c3
Hannan Bozkurt <hannanbozkurt@gmail.com>
2022-08-29 23:32:45 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   <!-- v-model bir değişkeni template kısmındaki bir input' veya benzer öğeye 
+   eşitlemek için kullannılan bir atrributedir -->
+    <input v-model="dataa" />
+    <!--String Interpolition vue js'te bir değişkenin değerini template içinde göstermek için bunu kullanırız -->
+    {{ dataa }}
+    <!--@click/v-on:click bir butona bir fonksiyon veya eylem tanımlanmak istendiği zaman bu şekilde kullanılır -->
+    <button @click="nmbr++">
+      {{ nmbr }}
+    </button>
+    {{ item }}
+    <button @click="setNumber()">besle carp</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  //DATA: değişkenlerin tanımlandığı yer
+  data() {
+    return {
+      dataa: null,
+      nmbr: 1,
+    };
+  },
+  //METHODS: Fonksiyon ve metodların tanımlandığı yer.
+  methods: {
+    setNumber() {
+      console.log("bes ile carptım carpmaz olaydım");
+      this.nmbr = this.nmbr * 3;
+    },
+  },
+  //En sık kullanılan küçük matematik işlemlerinin saklandığı yer
+  computed: {
+    item() {
+      return this.nmbr * 5;
+    },
+  },
+  //Component MOUNTE edilirken çalışması istenen fonksiyonlar ve yapılması istenen işlemler burada tutulur.
+  mounted() {
+    for (let i = 0; i < 5; i++) {
+      this.setNumber();
+    }
+    this.setNumber();
+    console.log(this);
+  },
+  //İçine yazılan daha önce tanımlı değişkenin anlık eventinı takip ederek istediğin aksiyonu alır
+  watch: {
+    dataa() {
+      console.log(this.dataa);
+    },
+  },
+};
 </script>
 
 <style>
